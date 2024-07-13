e6f4a48493df452753ede1f1ab1205f8cc177c11
RyomaOhtani <jukenohtani@gmail.com>
2024-07-14 00:34:49 +0900

    Delete unwanted lines

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -4,12 +4,6 @@ import COLOR from "../../variables/color"; // color.jsのCOLOR定数をインポ
 import TEXT from "../../variables/texts"; // text.jsのTEXT定数をインポート
 import FONTFAMILY from "../../variables/font_family"; // fontFamily.jsのFONTFAMILY定数をインポート
 import BREAKPOINT from "../../variables/breakpoint";
-/* styled-componentの定義 文字色にGREEN、背景色にLIGHT_BLACK、テキストのサイズはMを指定 */
-// const StyledExample = styled.div`
-//   color: ${COLOR.GREEN};
-//   background: ${COLOR.LIGHT_BLACK};
-//   ${TEXT.M}
-// `;
 
 export const Title = () => {
   return <StyledText>SIMPLE TODO APP</StyledText>;
