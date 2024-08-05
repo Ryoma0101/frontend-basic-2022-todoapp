e07ef676e40cd3cceb3a68a116e37c00b305b760
RyomaOhtani <jukenohtani@gmail.com>
2024-08-05 10:24:50 +0900

    CSS完了

--- src/components/Atoms/AddTaskButton/index.jsx
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -16,19 +16,36 @@ export const AddTaskButton = ({ onClick }) => {
 
 // styled-components の定義を追加
 const Button = styled.button`
-  background-color: ${COLOR.primary};
-  font-family: ${FONTFAMILY.primary};
-  /* その他のスタイル */
-`;
+  cursor: pointer;
+  display: inline-flex;
+  padding: 2px 6px;
+  align-items: center;
+  gap: 10px;
+  border-radius: 12px;
+  position: relative;
+  border: none;
+  background: none;
+  outline: none;
+  &:hover::before {
+    background: ${COLOR.GREEN};
+    opacity: 0.2;
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: 12px;
+    left: -1px;
+  }
+  `;
 
 const PlusButton = styled.img`
   width: 20px;
   height: 20px;
-  /* その他のスタイル */
-`;
+  `;
 
-const AddTaskText = styled.span`
-  color: ${COLOR.text};
-  font-size: ${TEXT.size};
-  /* その他のスタイル */
+const AddTaskText = styled.p`
+  margin: 0;
+  font-size: ${TEXT.S};
+  color: ${COLOR.GREEN};
+  font-family: ${FONTFAMILY.NOTO_SANS};
 `;
